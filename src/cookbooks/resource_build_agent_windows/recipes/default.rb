24d84a10a9028d2319989f3fdcc2952e9b6810fd
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-07-15 22:40:54 +1200

    Install java
    references #8

--- src/cookbooks/resource_build_agent_windows/recipes/default.rb
+++ src/cookbooks/resource_build_agent_windows/recipes/default.rb
@@ -16,6 +16,8 @@ include_recipe 'resource_build_agent_windows::firewall'
 
 include_recipe 'resource_build_agent_windows::meta'
 
+include_recipe 'resource_build_agent_windows::java'
+
 include_recipe 'resource_build_agent_windows::git'
 include_recipe 'resource_build_agent_windows::nuget'
 include_recipe 'resource_build_agent_windows::provisioning'
