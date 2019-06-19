d3d15b4f1d1beab88cc49a8d8cda3b6f6ef15f28
Petrik van der Velde <petrikvandervelde@gmail.com>
2019-06-19 14:13:01 +1200

    Enable long paths

--- src/cookbooks/resource_build_agent_windows/recipes/filesystem.rb
+++ src/cookbooks/resource_build_agent_windows/recipes/filesystem.rb
@@ -4,7 +4,26 @@
 # Cookbook Name:: resource_build_agent_windows
 # Recipe:: filesystem
 #
-# Copyright 2018, P. van der Velde
+# Copyright 2018, P. van der Veld
+#
+
+#
+# ENABLE LONG PATHS
+#
+
+# See: https://docs.microsoft.com/en-us/windows/desktop/fileio/naming-a-file#maximum-path-length-limitation
+registry_key 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem' do
+  action :create
+  recursive true
+  values [{
+    name: 'LongPathsEnabled',
+    type: :dword,
+    data: 1
+  }]
+end
+
+#
+# CREATE THE LANGUAGES DIRECTORY
 #
 
 languages_directory = node['paths']['languages']
@@ -17,6 +36,10 @@ temp_directory = node['paths']['temp']
     rights :modify, 'Administrators', applies_to_children: true
   end
 end
+
+#
+# CREATE THE SECRETS DIRECTORY
+#
 
 secrets_directory = node['paths']['secrets']
 directory secrets_directory do
--- src/cookbooks/resource_build_agent_windows/spec/filesystem_spec.rb
+++ src/cookbooks/resource_build_agent_windows/spec/filesystem_spec.rb
@@ -3,15 +3,26 @@
 require 'spec_helper'
 
 describe 'resource_build_agent_windows::filesystem' do
+  context 'allows long paths' do
+    let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+    # Note that the value - data is the MD5 hash of 0. See: https://github.com/chefspec/chefspec/issues/629
+    it 'sets the registry key' do
+      expect(chef_run).to create_registry_key('HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\FileSystem').with(
+        values: [{
+          name: 'LongPathsEnabled',
+          type: :dword,
+          data: '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b'
+        }]
+      )
+    end
+  end
+
   context 'create the base locations' do
     let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
-    it 'creates the tools directory' do
+    it 'creates the languages directory' do
       expect(chef_run).to create_directory('c:/languages')
-    end
-
-    it 'creates the tools directory' do
-      expect(chef_run).to create_directory('c:/tools')
     end
 
     it 'creates the secrets directory' do
